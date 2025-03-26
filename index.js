04f4c2c8c3be8ef1047f6e680be3ba6fec635efa
InnaBondarenko87 <141265858+InnaBondarenko87@users.noreply.github.com>
2025-03-26 16:35:22 +0000

    Deploying to gh-pages from @ InnaBondarenko87/test@3630dbce4a2b1e206ef89e9069db1a365b567222 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{a as o}from"./assets/helper-CcItgys7.js";/* empty css                      */console.log(o(5,2));
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=3e3,i=document.querySelector(".js-alert");i.addEventListener("click",c);let t=null;n();function n(){i.classList.add("is-visible"),t=setTimeout(()=>{console.log("setTimeout"),o()},e)}function c(){o(),clearTimeout(t)}function o(){i.classList.remove("is-visible")}
 //# sourceMappingURL=index.js.map
--- page-2.js
+++
@@ -1,2 +0,0 @@
-import{a as o}from"./assets/helper-CcItgys7.js";/* empty css                      */console.log(o(5,1));
-//# sourceMappingURL=page-2.js.map
