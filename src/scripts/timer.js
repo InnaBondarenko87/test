11df845f62bcc6db23abe34b3a99a10ea84e720e
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-03-28 09:43:01 +0100

    time_10modul

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,38 +1,71 @@
+/**
+ * Напишемо клас Timer, який буде
+ * запускати та зупиняти відлік часу
+ */
+
 const startBtn = document.querySelector('button[data-action-start]');
 const stopBtn = document.querySelector('button[data-action-stop]');
 const clockface = document.querySelector('.clockface');
 
 class Timer {
-  constructor() {
-    this.isActive;
-    return;
+  constructor({ onTick }) {
+    this.isActive = false;
+    this.onTick = onTick;
+    this.intervalId = null;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponent(0);
+    this.onTick(time);
   }
 
   start() {
+    if (this.isActive) {
+      return;
+    }
+
     const startTime = Date.now();
     this.isActive = true;
 
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - startTime;
       const time = this.getTimeComponent(deltaTime);
 
-      console.log(time);
+      this.onTick(time);
     }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponent(0);
+    this.onTick(time);
   }
 
   getTimeComponent(time) {
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    //61000 = 1xw 1 sek
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
     return { hours, mins, secs };
   }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-const time = new Timer();
-
-console.log(time);
+const time = new Timer({
+  onTick: updateClockface,
+});
 
 startBtn.addEventListener('click', time.start.bind(time));
+stopBtn.addEventListener('click', time.stop.bind(time));
+
+function updateClockface({ hours, mins, secs }) {
+  clockface.textContent = `${hours}:${mins}:${secs}`;
+}
