07838e03750debe531dd7e412447da28b63f0934
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-03-29 20:25:16 +0100

    add

---
+++ src/scripts/promises.js
@@ -0,0 +1,63 @@
+// const lalala = new Promise((resole, reject) => {
+//   const random = Math.random();
+
+//   //   setTimeout(() => {
+//   //     if (random > 0.5) {
+//   //       resole('ok');
+//   //     }
+
+//   //     reject('oops');
+//   //   }, 1000);
+
+//   if (random > 0.5) {
+//     resole('ok');
+//   }
+
+//   reject('oops');
+// });
+
+// lalala
+//   .then(result => {
+//     console.log('then', result);
+//   })
+//   .catch(error => {
+//     console.log('catch', error);
+//   })
+//   .finally(() => {
+//     console.log('finally');
+//   });
+// ===========================
+
+// const number = new Promise((resolve, reject) => {
+//   resolve(5);
+// });
+
+// number
+//   .then(data => {
+//     return data * 2;
+//   })
+//   .then(res => {
+//     return res * 3;
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log('ok');
+//   });
+// +++++++++++++++++++++++++++++++++
+
+const promise = new Promise(resolve => {
+  resolve(10);
+});
+
+promise
+  .then(value => {
+    new Promise(resolve => {
+      resolve(value * 2);
+    });
+  })
+  .then(value => console.log(value));
